fb61bb104edf38b647480ef5cf246d2db8d0544b
Jenbo <oliverjenbo677@gmail.com>
2025-02-10 10:49:25 +0100

    build a responsive layout

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,19 +1,23 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   return (
 
     <Grid
-    templateAreas={`"header header"
-      "nav main"
-      `}   
+    templateAreas={{
+
+      base: `"header" "main"`,
+      lg: `"header header" "aside main"`,
+    }}   
   >
   <GridItem pl='2' bg='orange.300' area={'header'}>
     Header
   </GridItem>
-  <GridItem pl='2' bg='pink.300' area={'nav'}>
-    Nav
-  </GridItem>
+  <Show above='lg'>
+    <GridItem pl='2' bg='pink.300' area={'aside'}>
+      aside
+    </GridItem>
+  </Show>
   <GridItem pl='2' bg='green.300' area={'main'}>
     Main
   </GridItem>
