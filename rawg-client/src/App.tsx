9ab6e20cbb87157f9dbcafd8430e8f0cef716ad0
Jenbo <oliverjenbo677@gmail.com>
2025-02-10 10:40:09 +0100

    Maybe fix?

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,6 +1,25 @@
-function App() {
-  return <>App</>;
+import { Grid, GridItem } from '@chakra-ui/react';
 
+function App() {
+  return (
+
+    <Grid
+    templateAreas={`"header header"
+      "nav main"
+      `}   
+  >
+  <GridItem pl='2' bg='orange.300' area={'header'}>
+    Header
+  </GridItem>
+  <GridItem pl='2' bg='pink.300' area={'nav'}>
+    Nav
+  </GridItem>
+  <GridItem pl='2' bg='green.300' area={'main'}>
+    Main
+  </GridItem>
+</Grid>
+
+  );
 }
 
 export default App;
