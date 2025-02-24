8b0341226722f88140d7bef4a4e49e9dfd515dc7
Jenbo <oliverjenbo677@gmail.com>
2025-02-24 09:56:42 +0100

    added spinner when loading

--- rawg-client/src/components/GenreList.tsx
+++ rawg-client/src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { Heading, HStack, List, ListItem, Text, Image } from "@chakra-ui/react";
+import { Heading, HStack, List, ListItem, Text, Image, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+
+if (error) return null;
+
+if (isLoading) return <Spinner />;
 
   return (
     <>
